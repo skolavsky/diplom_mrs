0b45afbeea263999072cb5a34cfa5bf3b0c50628
Ilya Rutski <teror714uns@gmail.com>
2023-12-11 13:42:07 +0300

    fix: method for validation in ClientForm

--- mrs_proj/clients/forms.py
+++ mrs_proj/clients/forms.py
@@ -20,6 +20,8 @@ class ClientForm(forms.ModelForm):
             'spo2': 'SPO2',
             'body_mass_index': 'ИМТ',
             'result': 'Исход',
+            'ch_d': 'Частота дыхания',
+            'cd_ozhir': 'Сахарный диабет или ожирение'
             # Add labels for other fields
         }
 
@@ -29,11 +31,20 @@ class ClientForm(forms.ModelForm):
             # Add widgets for other fields
         }
 
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        max_length = 100
+        if first_name and len(first_name) > max_length:
+            raise forms.ValidationError(f"Длина имени не должна превышать {max_length} символов.")
+
+        return first_name
+
     def clean_age(self):
         age = self.cleaned_data.get('age')
 
         if age and age < 0:
-            raise forms.ValidationError("Age must be a non-negative number.")
+            raise forms.ValidationError("Возраст не может быть отрицательным числом.")
 
         return age
 
@@ -41,22 +52,22 @@ class ClientForm(forms.ModelForm):
         body_mass_index = self.cleaned_data.get('body_mass_index')
 
         if body_mass_index and body_mass_index < 0:
-            raise forms.ValidationError("Body Mass Index must be a non-negative number.")
+            raise forms.ValidationError("ИМТ не может быть отрицательным числом.")
 
         return body_mass_index
 
     def clean_spo2(self):
         spo2 = self.cleaned_data.get('spo2')
 
-        if (spo2 and spo2 < 0) and spo2 <= 100:
-            raise forms.ValidationError("spo2 must be a non-negative number and < 100.")
+        if spo2 is not None and (spo2 < 0 or spo2 > 100):
+            raise forms.ValidationError("SPO2 должен быть в пределах от 0 до 100.")
 
         return spo2
 
     def clean_lf(self):
         lf = self.cleaned_data.get('lf')
 
-        if lf and lf < 0:
-            raise forms.ValidationError("lf must be a non-negative number.")
+        if lf is not None and lf < 0:
+            raise forms.ValidationError("lf не может быть отрицательным числом.")
 
         return lf
