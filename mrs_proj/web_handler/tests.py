75985f299dc00374880d2ed56e5ecbd6042380e4
Ilya Rutski <teror714uns@gmail.com>
2023-12-12 18:09:39 +0300

    docs: all for web_handler tests, fix: /login/

--- mrs_proj/web_handler/tests.py
+++ mrs_proj/web_handler/tests.py
@@ -1,57 +1,91 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 
+LOGIN = '/login/'
+
 
 class WebHandlerViewsTest(TestCase):
+    """
+    Класс для тестирования представлений (views) веб-приложения.
+    """
 
     def setUp(self):
-        # Тестовый юзер
+        # Создание тестового пользователя
         self.user = User.objects.create_user(username='testuser', password='testpassword')
 
-        # Create a test client
+        # Создание тестового клиента
         self.client = Client()
 
     def test_login_view(self):
-        # Test the GET request to the login view
-        response = self.client.get('/login/')
+        """
+        Тестирование представления для входа в систему (login view).
+
+        - Проверка GET-запроса к представлению входа в систему.
+        - Проверка POST-запроса с корректными учетными данными.
+        - Проверка POST-запроса с некорректными учетными данными.
+        """
+
+        # Проверка GET-запроса к представлению входа в систему
+        response = self.client.get(LOGIN)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'login.html')
 
-        # Test the POST request to the login view with valid credentials
-        response = self.client.post('/login/',
+        # Проверка POST-запроса с корректными учетными данными
+        response = self.client.post(LOGIN,
                                     {'action': 'login', 'username': 'testuser', 'password': 'testpassword'})
-        self.assertEqual(response.status_code, 302)  # Expect a redirect status code
+        self.assertEqual(response.status_code, 302)  # Ожидается код перенаправления
 
-        # Test the POST request to the login view with invalid credentials
-        response = self.client.post('/login/',
+        # Проверка POST-запроса с некорректными учетными данными
+        response = self.client.post(LOGIN,
                                     {'action': 'login', 'username': 'invaliduser', 'password': 'invalidpassword'})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'login.html')
         self.assertContains(response, 'Invalid login credentials')
 
     def test_contacts_view(self):
-        # Log in the user
+        """
+        Тестирование представления контактов (contacts view).
+
+        - Аутентификация пользователя.
+        - Проверка GET-запроса к представлению контактов.
+        """
+
+        # Аутентификация пользователя
         self.client.login(username='testuser', password='testpassword')
 
-        # Test the GET request to the contacts view
+        # Проверка GET-запроса к представлению контактов
         response = self.client.get('/contacts/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'contacts.html')
 
     def test_home_view(self):
-        # Log in the user
+        """
+        Тестирование представления домашней страницы (home view).
+
+        - Аутентификация пользователя.
+        - Проверка GET-запроса к представлению домашней страницы.
+        """
+
+        # Аутентификация пользователя
         self.client.login(username='testuser', password='testpassword')
 
-        # Test the GET request to the home view
+        # Проверка GET-запроса к представлению домашней страницы
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
 
     def test_dashboard_view(self):
-        # Log in the user
+        """
+        Тестирование представления панели управления (dashboard view).
+
+        - Аутентификация пользователя.
+        - Проверка GET-запроса к представлению панели управления.
+        """
+
+        # Аутентификация пользователя
         self.client.login(username='testuser', password='testpassword')
 
-        # Test the GET request to the dashboard view
+        # Проверка GET-запроса к представлению панели управления
         response = self.client.get('/dashboard/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'dashboard.html')
