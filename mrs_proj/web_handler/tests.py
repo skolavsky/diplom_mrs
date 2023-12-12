54151790c2483953eb4a131fd5d80ce5e35058c8
Ilya Rutski <teror714uns@gmail.com>
2023-12-12 17:57:31 +0300

    feature: web_handler: login test, all_menu pages tests

--- mrs_proj/web_handler/tests.py
+++ mrs_proj/web_handler/tests.py
@@ -1,5 +1,57 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
-class SimpleTest(TestCase):
-    def test_addition(self):
-        self.assertEqual(1 + 1, 2)
+
+class WebHandlerViewsTest(TestCase):
+
+    def setUp(self):
+        # Тестовый юзер
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+        # Create a test client
+        self.client = Client()
+
+    def test_login_view(self):
+        # Test the GET request to the login view
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'login.html')
+
+        # Test the POST request to the login view with valid credentials
+        response = self.client.post('/login/',
+                                    {'action': 'login', 'username': 'testuser', 'password': 'testpassword'})
+        self.assertEqual(response.status_code, 302)  # Expect a redirect status code
+
+        # Test the POST request to the login view with invalid credentials
+        response = self.client.post('/login/',
+                                    {'action': 'login', 'username': 'invaliduser', 'password': 'invalidpassword'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'login.html')
+        self.assertContains(response, 'Invalid login credentials')
+
+    def test_contacts_view(self):
+        # Log in the user
+        self.client.login(username='testuser', password='testpassword')
+
+        # Test the GET request to the contacts view
+        response = self.client.get('/contacts/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contacts.html')
+
+    def test_home_view(self):
+        # Log in the user
+        self.client.login(username='testuser', password='testpassword')
+
+        # Test the GET request to the home view
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_dashboard_view(self):
+        # Log in the user
+        self.client.login(username='testuser', password='testpassword')
+
+        # Test the GET request to the dashboard view
+        response = self.client.get('/dashboard/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'dashboard.html')
