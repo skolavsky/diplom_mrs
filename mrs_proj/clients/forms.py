2020997cfd7f03552de7075b5cf6265c63795959
Ilya Rutski <teror714uns@gmail.com>
2023-12-15 18:03:51 +0300

    feature: type checks for fields in clients-forms

--- mrs_proj/clients/forms.py
+++ mrs_proj/clients/forms.py
@@ -2,9 +2,10 @@ from django import forms
 from .models import Client
 from django.utils import timezone
 import re
-from decimal import Decimal, ROUND_DOWN
+from decimal import Decimal
+from datetime import date
 
-FIO_RE_VALIDATION = "^[A-Za-zА-Яа-яЁёЇїІіЄєҐґ' -]+$"
+FIO_RE_VALIDATION = "^[A-Za-zА-Яа-яЁё' -]+$"
 FIO_MAX_LENGTH = 100
 
 
@@ -44,22 +45,32 @@ class ClientForm(forms.ModelForm):
             'first_name': forms.TextInput(attrs={'style': 'width: 80%;'}),
             'admission_date': forms.SelectDateWidget(),
             'comorb_ccc': forms.CheckboxInput(attrs={'class': 'checkbox-input'}),
-            # Add widgets for other fields
         }
 
     def clean_first_name(self):
         first_name = self.cleaned_data.get('first_name')
-        if first_name and len(first_name) > FIO_MAX_LENGTH:
-            raise forms.ValidationError(f"Длина имени не должна превышать {FIO_MAX_LENGTH} символов.")
 
-        # Проверка на наличие цифр, знаков, кроме апострофа и дефиса
-        if first_name and not re.match(FIO_RE_VALIDATION, first_name):
-            raise forms.ValidationError("Имя может содержать только буквы, апостроф, дефис и пробел.")
+        if first_name is not None:
+            if not isinstance(first_name, str):
+                raise forms.ValidationError("Имя должно быть строкой.")
+
+            # Проверка на длину не более 100 символов
+            max_length = 100
+            if len(first_name) > max_length:
+                raise forms.ValidationError(f"Длина имени не должна превышать {max_length} символов.")
+
+            # Проверка на наличие цифр, знаков, кроме апострофа и дефиса
+            if not re.match(FIO_RE_VALIDATION, first_name):
+                raise forms.ValidationError("Имя может содержать только буквы, апостроф, дефис и пробел.")
 
         return first_name
 
     def clean_last_name(self):
         last_name = self.cleaned_data.get('last_name')
+
+        if last_name is not None:
+            if not isinstance(last_name, str):
+                raise forms.ValidationError("Фамилия должна быть строкой.")
 
         # Проверка на длину не более 100 символов
         if last_name and len(last_name) > FIO_MAX_LENGTH:
@@ -74,6 +85,10 @@ class ClientForm(forms.ModelForm):
     def clean_patronymic(self):
         patronymic = self.cleaned_data.get('patronymic')
 
+        if patronymic is not None:
+            if not isinstance(patronymic, str):
+                raise forms.ValidationError("Отчество должно быть строкой.")
+
         if patronymic and len(patronymic) > FIO_MAX_LENGTH:
             raise forms.ValidationError(f"Длина отчества не должна превышать {FIO_MAX_LENGTH} символов.")
 
@@ -86,7 +101,9 @@ class ClientForm(forms.ModelForm):
         age = self.cleaned_data.get('age')
 
         if age is not None:
-            if age < 0:
+            if not isinstance(age, (int, float)):
+                raise forms.ValidationError("Возраст должен быть числом.")
+            elif age < 0:
                 raise forms.ValidationError("Возраст не может быть отрицательным числом.")
             elif age > 120:  # Установите желаемое верхнее значение, например, 150 лет
                 raise forms.ValidationError("Возраст не может превышать 120 лет.")
@@ -96,16 +113,19 @@ class ClientForm(forms.ModelForm):
     def clean_body_mass_index(self):
         body_mass_index = self.cleaned_data.get('body_mass_index')
 
-        # Проверка на отрицательное значение BMI
-        if body_mass_index is not None and body_mass_index < 0.0:
-            raise forms.ValidationError("Индекс массы тела не может быть отрицательным.")
-
-        # Проверка на значение BMI не более 100.0
-        if body_mass_index is not None and body_mass_index > 100.0:
-            raise forms.ValidationError("Индекс массы тела не может быть более 100.0.")
-
-        # Проверка на количество знаков после запятой (не более 3)
         if body_mass_index is not None:
+            if not isinstance(body_mass_index, (int, float)):
+                raise forms.ValidationError("Индекс массы тела должен быть числом.")
+
+            # Проверка на отрицательное значение BMI
+            if body_mass_index < 0.0:
+                raise forms.ValidationError("Индекс массы тела не может быть отрицательным.")
+
+            # Проверка на значение BMI не более 100.0
+            if body_mass_index > 100.0:
+                raise forms.ValidationError("Индекс массы тела не может быть более 100.0.")
+
+            # Проверка на количество знаков после запятой (не более 3)
             decimal_part = Decimal(str(body_mass_index)).as_tuple().exponent
             if decimal_part < -3:
                 raise forms.ValidationError("Индекс массы тела может иметь не более 3 знаков после запятой.")
@@ -115,21 +135,33 @@ class ClientForm(forms.ModelForm):
     def clean_spo2(self):
         spo2 = self.cleaned_data.get('spo2')
 
-        if spo2 is not None and (spo2 < 0 or spo2 > 100):
-            raise forms.ValidationError("SPO2 должен быть в пределах от 0 до 100.")
+        if spo2 is not None:
+            if not isinstance(spo2, int):
+                raise forms.ValidationError("SPO2 должен быть целым числом.")
+
+            if spo2 < 0 or spo2 > 100:
+                raise forms.ValidationError("SPO2 должен быть в пределах от 0 до 100.")
 
         return spo2
 
     def clean_lf(self):
         lf = self.cleaned_data.get('lf')
 
-        if lf is not None and lf < 0:
-            raise forms.ValidationError("lf не может быть отрицательным числом.")
+        if lf is not None:
+            if not isinstance(lf, int):
+                raise forms.ValidationError("lf должен быть числом.")
+
+            if lf < 0:
+                raise forms.ValidationError("lf не может быть отрицательным числом.")
 
         return lf
 
     def clean_admission_date(self):
         admission_date = self.cleaned_data.get('admission_date')
+
+        # Проверка формата даты
+        if not isinstance(admission_date, date):
+            raise forms.ValidationError("Некорректный формат даты.")
 
         if admission_date:
             # Проверка на дату, не больше сегодняшнего дня
@@ -146,6 +178,10 @@ class ClientForm(forms.ModelForm):
         ch_d = self.cleaned_data.get('ch_d')
 
         # Проверка на отрицательное значение ch_d
+        if ch_d is not None and not isinstance(ch_d, int):
+            raise forms.ValidationError("Частота дыхания должна быть целым числом.")
+
+        # Проверка на отрицательное значение ch_d
         if ch_d is not None and ch_d < 0:
             raise forms.ValidationError("Частота дыхания не может быть отрицательной.")
 
@@ -159,17 +195,24 @@ class ClientForm(forms.ModelForm):
         f_test_ex = self.cleaned_data.get('f_test_ex')
 
         # Проверка на отрицательное значение f_test_ex
+        if f_test_ex is not None and not isinstance(f_test_ex, int):
+            raise forms.ValidationError("Значение f_test_ex должно быть целым числом.")
+
+        # Проверка на отрицательное значение f_test_ex
         if f_test_ex is not None and f_test_ex < 0:
             raise forms.ValidationError("Значение f_test_ex не может быть отрицательным.")
 
         # Проверка на значение f_test_ex не более 200
         if f_test_ex is not None and f_test_ex > 200:
             raise forms.ValidationError("Значение f_test_ex не может превышать 200.")
-
         return f_test_ex
 
     def clean_f_test_in(self):
         f_test_in = self.cleaned_data.get('f_test_in')
+
+        # Проверка на отрицательное значение f_test_in
+        if f_test_in is not None and not isinstance(f_test_in, int):
+            raise forms.ValidationError("Значение f_test_in должно быть целым числом.")
 
         # Проверка на отрицательное значение f_test_in
         if f_test_in is not None and f_test_in < 0:
@@ -184,18 +227,26 @@ class ClientForm(forms.ModelForm):
     def clean_l_109(self):
         l_109 = self.cleaned_data.get('l_109')
 
+        # Проверка на тип данных (float)
+        if l_109 is not None and not isinstance(l_109, float):
+            raise forms.ValidationError("Значение l_109 должно быть числом с плавающей точкой.")
+
         # Проверка на отрицательное значение l_109
         if l_109 is not None and l_109 < 0:
             raise forms.ValidationError("Значение l_109 не может быть отрицательным.")
 
         # Проверка на значение l_109 не более 50
-        if l_109 is not None and l_109 > 50:
+        if l_109 is not None and l_109 > 50.0:
             raise forms.ValidationError("Значение l_109 не может превышать 50.")
 
         return l_109
 
     def clean_lf(self):
         lf = self.cleaned_data.get('lf')
+
+        # Проверка на тип данных (int)
+        if lf is not None and not isinstance(lf, int):
+            raise forms.ValidationError("Значение lf должно быть целым числом.")
 
         # Проверка на отрицательное значение lf
         if lf is not None and lf < 0:
@@ -210,6 +261,10 @@ class ClientForm(forms.ModelForm):
     def clean_rox(self):
         rox = self.cleaned_data.get('rox')
 
+        # Проверка на тип данных (float)
+        if rox is not None and not isinstance(rox, float):
+            raise forms.ValidationError("Значение rox должно быть числом с плавающей запятой.")
+
         # Проверка на отрицательное значение rox
         if rox is not None and rox < 0:
             raise forms.ValidationError("Значение rox не может быть отрицательным.")
@@ -222,6 +277,10 @@ class ClientForm(forms.ModelForm):
 
     def clean_spo2_fio(self):
         spo2_fio = self.cleaned_data.get('spo2_fio')
+
+        # Проверка на тип данных (float)
+        if spo2_fio is not None and not isinstance(spo2_fio, float):
+            raise forms.ValidationError("Значение spo2_fio должно быть числом с плавающей запятой.")
 
         # Проверка на отрицательное значение spo2_fio
         if spo2_fio is not None and spo2_fio < 0:
