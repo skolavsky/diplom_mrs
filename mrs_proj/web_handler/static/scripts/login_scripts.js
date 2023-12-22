02ad94dd6884608996104140d80904c9bd3f1e2f
ElGansoDorado <yakovchikdenis@gmail.com>
2023-12-23 02:24:24 +0300

    update: login.html and file client_list.html in clients

--- mrs_proj/web_handler/static/scripts/login_scripts.js
+++ mrs_proj/web_handler/static/scripts/login_scripts.js
@@ -1,20 +1,19 @@
-// hello/static/scripts/script.js
-document.addEventListener('DOMContentLoaded', function () {
-    // Wait for the DOM content to be fully loaded before executing the script
+function show_hide_password(target){
+	var input = document.getElementById('password');
+    var element = document.getElementById('eye');
 
-    const  showPasswordButton = document.getElementById('showPassword');
-    const  passwordInput = document.getElementById('password');
+	if (input.getAttribute('type') == 'password') 
+    {
+		target.classList.add('view');
+		input.setAttribute('type', 'text');
+        element.className = 'fas fa-eye';
+	} 
+    else 
+    {
+		target.classList.remove('view');
+		input.setAttribute('type', 'password');
+        element.className = 'fas fa-eye-slash';
+	}
 
-    if (showPasswordButton && passwordInput) {
-        // Check if the elements are present on the page
-
-        showPasswordButton.addEventListener('input', function () { // NOSONAR
-            // Toggle the type attribute of the password input
-            if (passwordInput.type === 'password') { // NOSONAR
-                passwordInput.type = 'text';  // NOSONAR
-            } else { // NOSONAR
-                passwordInput.type = 'password'; // NOSONAR
-            } // NOSONAR
-        }); // NOSONAR
-    } // NOSONAR
-});  // NOSONAR
+	return false;
+}
