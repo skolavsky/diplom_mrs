a8acccfd9868064fe681fbd1fffd6ba28f62ed25
Ilya Rutski <teror714uns@gmail.com>
2024-01-01 20:40:38 +0300

    feature: client model registration in admin panel
    клиент приложение в админке

--- mrs_proj/clients/admin.py
+++ mrs_proj/clients/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'patronymic', 'age', 'gender', 'admission_date')
+    search_fields = ('first_name', 'last_name', 'patronymic')
+    list_filter = ('gender', 'admission_date')
+
+
+admin.site.register(Client, ClientAdmin)
