9bb1bf258b0678b439a75974a71efb3ad418850e
Ilya Rutski <teror714uns@gmail.com>
2024-01-01 21:09:20 +0300

    feature:
    добавил функционал для расширения базы через админку окончательно.
    теперь, чтобы добавить записи нужно выбрать уже существующие записи.
    и по кол-ву записей выбранных будет добавлено столько же тестовых полей.
    там также действуют ограничения. мин: 1, макс 100 записей к добавлению.

--- mrs_proj/clients/admin.py
+++ mrs_proj/clients/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
-from .models import Client
+from clients.models import Client
+from .management.commands.populate_data import Command as PopulateDataCommand
 
 
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'patronymic', 'age', 'gender', 'admission_date')
+    list_display = ('first_name', 'last_name', 'patronymic', 'age', 'gender', 'admission_date')
     search_fields = ('first_name', 'last_name', 'patronymic')
-    list_filter = ('gender', 'admission_date')
+    list_filter = ('gender', 'admission_date', 'spo2')
+
+    actions = ['generate_test_data']
+
+    def generate_test_data(self, request, oper_data):
+        count = len(oper_data)
+        populate_command = PopulateDataCommand()
+        populate_command.handle(count=count)
+        self.message_user(request, f'Successfully populated the database with {count} Client test data.')
+
+    generate_test_data.short_description = 'Generate Test Data'
 
 
 admin.site.register(Client, ClientAdmin)
