714b52edfc26ae16048f58f9ca287e6dd138e831
Ilya Rutski <teror714uns@gmail.com>
2024-01-01 20:35:02 +0300

    update: values ​​for creating records were adapted to the validation boundaries
    значения для создания записей были адаптированы под границы валидации

--- mrs_proj/clients/management/commands/populate_data.py
+++ mrs_proj/clients/management/commands/populate_data.py
@@ -6,7 +6,10 @@ import secrets
 from datetime import date
 import argparse
 
+START_YEAR = 2023
+
 fake = Faker('ru_RU')
+
 
 class Command(BaseCommand):
     help = 'Populate the database with fake data for Client model'
@@ -34,25 +37,25 @@ class Command(BaseCommand):
                 id=uuid.uuid4(),
                 first_name=fake.first_name(),
                 last_name=fake.last_name(),
-                patronymic=fake.last_name(),
-                age=fake.random_int(min=18, max=80),
-                body_mass_index=fake.pyfloat(),
-                spo2=fake.random_int(min=90, max=100),
-                admission_date=fake.date_between(start_date='-30d', end_date='today'),
+                patronymic=fake.middle_name(),
+                age=fake.random_int(min=0, max=120),
+                body_mass_index=fake.pyfloat(min_value=0, max_value=100.0, right_digits=3),
+                spo2=fake.random_int(min=0, max=100),
+                admission_date=fake.date_between_dates(date(START_YEAR, 1, 1), date.today()),
                 result=fake.random_int(min=0, max=100),
-                dayshome=fake.random_int(min=1, max=30),
+                dayshome=fake.random_int(min=0, max=50),
                 gender=fake.random_element(elements=gender_choices),
-                f_test_ex=fake.random_int(min=0, max=10, step=1),
-                f_test_in=fake.random_int(min=0, max=10, step=1),
+                f_test_ex=fake.random_int(min=0, max=200),
+                f_test_in=fake.random_int(min=0, max=200),
                 comorb_ccc=fake.random_element(elements=comorb_choices),
                 comorb_bl=fake.random_element(elements=comorb_choices),
                 cd_ozhir=fake.random_element(elements=comorb_choices),
                 comorb_all=fake.random_element(elements=comorb_choices),
-                l_109=fake.pyfloat(),
-                lf=fake.pyfloat(),
-                rox=fake.pyfloat(),
-                spo2_fio=fake.pyfloat(),
-                ch_d=fake.random_int(min=0, max=100),
+                l_109=fake.pyfloat(min_value=0, max_value=50.0, right_digits=3),
+                lf=fake.pyfloat(min_value=0, max_value=50.0, right_digits=3),
+                rox=fake.pyfloat(min_value=0, max_value=50.0, right_digits=3),
+                spo2_fio=fake.pyfloat(min_value=0, max_value=600.0, right_digits=3),
+                ch_d=fake.random_int(min=0, max=150),
             )
 
         self.stdout.write(self.style.SUCCESS(f'Successfully populated the database with {count} Client test data'))
