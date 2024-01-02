5606a3758078d5d19411ae63ffe821c1a6bfec32
Ilya Rutski <teror714uns@gmail.com>
2024-01-02 15:17:47 +0300

    feature: registered the team in the admin panel
    зарегистрировал команду в админ-панеле. можно выбрать для каких элементов. для всех там можно легко выбрать.

--- mrs_proj/clients/admin.py
+++ mrs_proj/clients/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from clients.models import Client
 from .management.commands.populate_data import Command as PopulateDataCommand
+from django.core.management import call_command
 
 
 class ClientAdmin(admin.ModelAdmin):
@@ -8,13 +9,19 @@ class ClientAdmin(admin.ModelAdmin):
     search_fields = ('first_name', 'last_name', 'patronymic')
     list_filter = ('gender', 'admission_date', 'spo2')
 
-    actions = ['generate_test_data']
+    actions = ['generate_test_data', 'regenerate_tokens']
 
     def generate_test_data(self, request, oper_data):
         count = len(oper_data)
         populate_command = PopulateDataCommand()
         populate_command.handle(count=count)
         self.message_user(request, f'Successfully populated the database with {count} Client test data.')
+
+    def regenerate_tokens(self, request, queryset):
+        selected_clients = queryset.values_list('id', flat=True)
+        call_command('regenerate_tokens', *selected_clients)
+
+    regenerate_tokens.short_description = 'Regenerate Tokens for Selected Clients'
 
     generate_test_data.short_description = 'Generate Test Data'
 
