398cd2e9f1ee97fd035b472d3d0acbafcb500c67
Ilya Rutski <teror714uns@gmail.com>
2024-01-28 21:21:15 +0300

    fix: test method for api
    разкоментировал тот тестовый метод. зачем он нужен так и не понял, но пускай пока побудет

--- mrs_proj/clients/AI/views.py
+++ mrs_proj/clients/AI/views.py
@@ -1,8 +1,9 @@
+import concurrent
+import time
+from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.response import Response
-import secrets
 from rest_framework.views import APIView
-
 import secrets
 
 
@@ -21,15 +22,14 @@ class ResultAPI(APIView):
         return Response(data=data, status=status.HTTP_200_OK)
 
 
-"""
-def post_test(request):
+def post_test(requests):
     def send_request(url):
         response = requests.post(url, data={"name": "Test", "age": 69})
         if response.status_code == 200:
             return True
         else:
             return False
-    
+
     url = "http://127.0.0.1:8000/api/"
     concurrency = 700
     total_requests = 700
@@ -50,5 +50,5 @@ def post_test(request):
     for result in results:
         if result.status_code != 200:
             failed += 1
-    return HttpResponse(f"Successful requests: {successful_requests}/{total_requests} in {time.time() - start_time}\nfailed: {failed}")
-"""
+    return HttpResponse(
+        f"Successful requests: {successful_requests}/{total_requests} in {time.time() - start_time}\nfailed: {failed}")
