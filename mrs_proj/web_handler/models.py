a82537c1b96289c4fba878360ad5b54f3e7ca1bb
Ilya Rutski <teror714uns@gmail.com>
2024-01-29 19:22:16 +0300

    feature: model for dashboard
    модель для статей

--- mrs_proj/web_handler/models.py
+++ mrs_proj/web_handler/models.py
@@ -1,3 +1,21 @@
+# web_handler.models.py
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+
+class Article(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = uuid.uuid4()
+        super(Article, self).save(*args, **kwargs)
