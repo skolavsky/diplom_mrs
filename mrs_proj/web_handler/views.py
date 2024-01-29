742b884f79316deb59d137340a4b83b8309557dd
Ilya Rutski <teror714uns@gmail.com>
2024-01-29 19:22:52 +0300

    feature: view for new dashboard-ajax functions
    сделал вьюху для страницы статей с ajax

--- mrs_proj/web_handler/views.py
+++ mrs_proj/web_handler/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse, HttpResponseBadRequest
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from datetime import timedelta
 from django.utils import timezone
-
 from clients.models import ClientData
+from .models import Article
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 LOGIN_URL = '/login/'
 
@@ -36,13 +38,33 @@ class HomeView(LoginRequiredMixin, View):
 
         return render(request, 'home.html', context)
 
-
 class DashboardView(LoginRequiredMixin, View):
     login_url = LOGIN_URL
 
     def get(self, request):
-        return render(request, 'dashboard.html')
+        articles = Article.objects.all()
+        return render(request, 'dashboard.html', {'articles': articles})
 
+    def load_articles(self, request):
+        offset = int(request.GET.get('offset', 0))
+        articles = Article.objects.all()[offset:offset+10]
+        data = {'articles': []}
+        for article in articles:
+            data['articles'].append({
+                'id': str(article.id),
+                'author': article.author.username,
+                'title': article.title,
+                'content': article.content,
+                'created_at': article.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+                'last_modified_at': article.last_modified_at.strftime('%Y-%m-%d %H:%M:%S'),
+                'note_id': str(article.note_id),
+            })
+        return JsonResponse(data)
+
+    def dispatch(self, *args, **kwargs):
+        if self.request.headers.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+            return self.load_articles(self.request)
+        return super().dispatch(*args, **kwargs)
 
 class ContactsView(LoginRequiredMixin, View):
     login_url = LOGIN_URL
