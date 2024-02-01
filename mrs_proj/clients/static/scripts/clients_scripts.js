1fd72efb3254eec5681b460f84df6ca31f88c7b7
Ilya Rutski <teror714uns@gmail.com>
2024-02-01 22:43:02 +0300

    refactor: scripts to js file
    перенес все скрипты, кроме одного, который для страниц, который используется шаблонизатор в отдельный файл cleints_scripts.js

--- mrs_proj/clients/static/scripts/clients_scripts.js
+++ mrs_proj/clients/static/scripts/clients_scripts.js
@@ -13,6 +13,16 @@ function cancelAddClient() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    const resetSortingButton = document.getElementById('resetSortingButton');
+
+    resetSortingButton.addEventListener('click', function () {
+        const currentUrl = new URL(window.location.href);
+        currentUrl.searchParams.delete('sort');  // Remove the 'sort' parameter
+        window.location.href = currentUrl.toString();
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
     const searchInput = document.getElementById('searchInput');
     const searchButton = document.getElementById('searchButton');
 
@@ -25,8 +35,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-
-document.addEventListener('DOMContentLoaded', function () {
     const resetSortingButton = document.getElementById('resetSortingButton');
 
     resetSortingButton.addEventListener('click', function () {
@@ -40,42 +48,18 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-// Function to validate the client form
-function validateClientForm() {
-    // Get form inputs
-    let firstName = $('#id_first_name').val().trim();
-    let lastName = $('#id_last_name').val().trim();
-    let patronymic = $('#id_patronymic').val().trim();
-    let age = $('#id_age').val().trim();
 
-    // Validate first name
-    if (!firstName) {
-        alert('Please enter a valid first name.');
-        return false;
-    }
-
-    // Validate last name
-    if (!lastName) {
-        alert('Please enter a valid last name.');
-        return false;
-    }
-
-    // Validate patronymic
-    if (!patronymic) {
-        alert('Please enter a valid patronymic.');
-        return false;
-    }
-
-    // Validate age (should be a positive integer)
-    if (!age || isNaN(age) || parseInt(age) < 0) {
-        alert('Please enter a valid age.');
-        return false;
-    }
-
-    // If all validations pass, return true
-    return true;
+function openGoToPageModal() {
+    document.getElementById('goToPageModal').style.display = 'block';
 }
 
+function closeGoToPageModal() {
+    document.getElementById('goToPageModal').style.display = 'none';
+}
 
-
-
+window.onclick = function (event) {
+    var modal = document.getElementById('goToPageModal');
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+};
