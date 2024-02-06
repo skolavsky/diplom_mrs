f2d67dc2ab5d02b04baa50e4787cd399d5663328
Галанин Сергей <galaninss.del@gmail.com>
2024-02-06 13:08:44 +0300

    feat: test

--- mrs_proj/clients/AI/tests.py
+++ mrs_proj/clients/AI/tests.py
@@ -10,20 +10,25 @@ API_URL = reverse('result')
 class ResultAPITest(APITestCase):
     def setUp(self) -> None:
         self.client = APIClient()
-        user = User.objects.create_user(username='testuser', password='testpass')
-        self.client.force_authenticate(user=user)
+        self.user = User.objects.create_user(username='testuser', password='testpass')
 
     def test_get(self):
         response = self.client.get(API_URL)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_post(self):
+        self.client.force_authenticate(user=None)
         response = self.client.post(API_URL)
         self.assertIn(
             response.status_code,
-            [status.HTTP_200_OK,
-             status.HTTP_403_FORBIDDEN,
-             status.HTTP_401_UNAUTHORIZED]
+            [status.HTTP_403_FORBIDDEN,
+            status.HTTP_401_UNAUTHORIZED]
+        )
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(API_URL)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
              )
         result = json.loads(response.content).get('result')
         self.assertIn(result, ["Прогноз: 1", "Прогноз: 2"])
