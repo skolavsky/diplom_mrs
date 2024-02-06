a123d138fa88f24548ae296e61df03125750f86d
Ilya Rutski <teror714uns@gmail.com>
2024-02-06 21:19:24 +0300

    fix: installed appliacations, styles
    убрал штуку, которую думал юзать раньше, исправил стили

--- mrs_proj/mrs_proj/settings_common.py
+++ mrs_proj/mrs_proj/settings_common.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'querystring_tag',
     'web_handler',
     'clients',
     'simple_history',
