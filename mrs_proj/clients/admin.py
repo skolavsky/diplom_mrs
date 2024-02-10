99bf1005c5336894229b86d5401215c29d7dd289
Ilya Rutski <teror714uns@gmail.com>
2024-02-10 18:39:59 +0300

    refactor: reroll uuid token command delete
    удалил бесполезную команду по сбросу токена.

--- mrs_proj/clients/admin.py
+++ mrs_proj/clients/admin.py
@@ -1,17 +1,31 @@
 # clients.admin.py
-from django.contrib import admin
 from .models import PersonalInfo, ClientData
+from django.contrib import admin
+from clients.models import ClientData
+from .management.commands.populate_data import Command as PopulateDataCommand
+from django.core.management import call_command
+from io import BytesIO, StringIO
+from django.http import HttpResponse
 
 
 @admin.register(PersonalInfo)
-class ClientDataAdmin(admin.ModelAdmin):
+class PersonalInfoAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'patronymic', 'gender', 'is_active', 'id', ]
     list_filter = ['gender', 'is_active']
     search_fields = ['first_name', 'last_name', 'patronymic', 'gender']
+    actions = ['generate_test_data']
+
+    def generate_test_data(self, request, oper_data):
+        count = len(oper_data)
+        populate_command = PopulateDataCommand()
+        populate_command.handle(count=count)
+        self.message_user(request, f'Successfully populated the database with {count} Client test data.')
+
+    generate_test_data.short_description = 'Создать тестовых пользователей.'
 
 
 @admin.register(ClientData)
-class PersonalInfoAdmin(admin.ModelAdmin):
+class ClientDataAdmin(admin.ModelAdmin):
     list_display = ['personal_info', 'age', 'body_mass_index', 'spo2', 'admission_date', 'result', 'dayshome', 'rox',
                     'f_test_ex', 'f_test_in', 'comorb_ccc', 'comorb_bl', 'cd_ozhir', 'ch_d', 'lf', 'l_109', 'spo2_fio',
                     'date_added', ]
