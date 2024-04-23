23038b4f8aa4cdeb743bdff06d41ab1ce0046de9
Ilya Rutski <teror714uns@gmail.com>
2024-04-23 22:46:20 +0300

    feature: image
    добавил аву для пользователей(пока не работает)

--- mrs_proj/web_handler/admin.py
+++ mrs_proj/web_handler/admin.py
@@ -1,6 +1,6 @@
 # web_handler.admin.py
 from django.contrib import admin
-from .models import Article
+from .models import Article, Profile
 
 
 class ArticleAdmin(admin.ModelAdmin):
@@ -10,3 +10,11 @@ class ArticleAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Article, ArticleAdmin)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'bio', 'telegram_id', 'is_white_list', 'location', 'birth_date', 'avatar']
+    # Добавьте любые другие поля, которые вы хотите видеть в админке
+
+
+admin.site.register(Profile, ProfileAdmin)
--- mrs_proj/web_handler/models.py
+++ mrs_proj/web_handler/models.py
@@ -1,10 +1,16 @@
 # web_handler.models.py
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
 from django.dispatch import receiver
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.contrib.auth import get_user_model
+from django.core.validators import FileExtensionValidator, MaxValueValidator
+from PIL import Image
+import os
+from django.core.files.base import ContentFile
 
 
 class Article(models.Model):
@@ -24,13 +30,45 @@ class Article(models.Model):
         super(Article, self).save(*args, **kwargs)
 
 
+def user_directory_path(instance, filename):
+    return 'user_{0}/avatar/{1}'.format(instance.user.id, filename)
+
+
+def validate_image(image):
+    file_size = image.file.size
+    limit_mb = 2
+    if file_size > limit_mb * 1024 * 1024:
+        raise ValidationError("Max file size is %sMB" % limit_mb)
+    else:
+        img = Image.open(image)
+        width, height = img.size
+        max_width = 800
+        max_height = 600
+        if width > max_width or height > max_height:
+            raise ValidationError("Max dimensions allowed are {0}x{1} pixels".format(max_width, max_height))
+
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     telegram_id = models.TextField(max_length=100, blank=True)
     is_white_list = models.BooleanField(default=False)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+    avatar = models.ImageField(upload_to=user_directory_path, blank=True, null=True,
+                               validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png']),
+                                           MaxValueValidator(2 * 1024 * 1024), validate_image])
+
+    def save(self, *args, **kwargs):
+        if self.avatar:
+            if isinstance(self.avatar, ContentFile):
+                # Получаем имя файла
+                file_name = os.path.basename(self.avatar.name)
+                # Получаем содержимое файла
+                file_content = self.avatar.read()
+                # Записываем содержимое файла обратно в поле аватара
+                self.avatar.save(file_name, ContentFile(file_content), save=False)
+        super().save(*args, **kwargs)
 
 
 @receiver(post_save, sender=get_user_model())
