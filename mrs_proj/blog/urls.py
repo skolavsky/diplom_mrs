3aafd05fe2f492cfdc190b464856d1372ba5f2e8
Ilya Rutski <teror714uns@gmail.com>
2024-04-28 19:38:06 +0300

    refactor: blog view
    переделал view чтобы работали через классы

--- mrs_proj/blog/urls.py
+++ mrs_proj/blog/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 
-from . import views
+from .views import PostListView, PostDetailView, PostSearchView
 
 app_name = 'blog'
 
 # Post views
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    # path('', views.PostListView.as_view(), name='post_list'),
-    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
-    path('search/', views.post_search, name='post_search'),
+    path('', PostListView.as_view(), name='post_list'),
+    path('tag/<slug:tag_slug>/', PostListView.as_view(), name='post_list_by_tag'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', PostDetailView.as_view(), name='post_detail'),
+    path('search/', PostSearchView.as_view(), name='post_search'),
 ]
--- mrs_proj/blog/views.py
+++ mrs_proj/blog/views.py
@@ -1,75 +1,67 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.postgres.search import TrigramSimilarity
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
+from django.views import View
 from taggit.models import Tag
 
 from .forms import SearchForm
 from .models import Post
 
+class PostListView(LoginRequiredMixin, View):
+    def get(self, request, tag_slug=None):
+        post_list = Post.published.all()
+        tag = None
+        if tag_slug:
+            tag = get_object_or_404(Tag, slug=tag_slug)
+            post_list = post_list.filter(tags__in=[tag])
+        paginator = Paginator(post_list, 3)
+        page_number = request.GET.get('page', 1)
+        try:
+            posts = paginator.page(page_number)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
+        return render(request,
+                      'blog/post/list.html',
+                      {'posts': posts, 'tag': tag})
 
-@login_required
-def post_list(request, tag_slug=None):
-    post_list = Post.published.all()
-    tag = None
-    if tag_slug:
-        tag = get_object_or_404(Tag, slug=tag_slug)
-        post_list = post_list.filter(tags__in=[tag])
-    # Постраничная разбивка с 3 постами на страницу
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # Если page_number не целое число, то
-        # выдать первую страницу
-        posts = paginator.page(1)
-    except EmptyPage:
-        # Если page_number находится вне диапазона, то
-        # выдать последнюю страницу
-        posts = paginator.page(paginator.num_pages)
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts, 'tag': tag})
+class PostSearchView(LoginRequiredMixin, View):
+    def get(self, request):
+        form = SearchForm()
+        query = None
+        results = []
 
+        if 'query' in request.GET:
+            form = SearchForm(request.GET)
+            if form.is_valid():
+                query = form.cleaned_data['query']
+                results = Post.published.annotate(
+                    similarity=TrigramSimilarity('title', query),
+                ).filter(similarity__gt=0.1).order_by('-similarity')
 
-@login_required
-def post_search(request):
-    form = SearchForm()
-    query = None
-    results = []
+        return render(request,
+                      'blog/post/search.html',
+                      {'form': form,
+                       'query': query,
+                       'results': results})
 
-    if 'query' in request.GET:
-        form = SearchForm(request.GET)
-        if form.is_valid():
-            query = form.cleaned_data['query']
-            results = Post.published.annotate(
-                similarity=TrigramSimilarity('title', query),
-            ).filter(similarity__gt=0.1).order_by('-similarity')
+class PostDetailView(LoginRequiredMixin, View):
+    def get(self, request, year, month, day, post):
+        post = get_object_or_404(Post,
+                                 status=Post.Status.PUBLISHED,
+                                 slug=post,
+                                 publish__year=year,
+                                 publish__month=month,
+                                 publish__day=day)
 
-    return render(request,
-                  'blog/post/search.html',
-                  {'form': form,
-                   'query': query,
-                   'results': results})
+        post_tags_ids = post.tags.values_list('id', flat=True)
+        similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+        similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
 
-
-@login_required
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post,
-                             status=Post.Status.PUBLISHED,
-                             slug=post,
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
-
-    # List of similar posts
-    post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
-
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post': post,
-                   'similar_posts': similar_posts})
+        return render(request,
+                      'blog/post/detail.html',
+                      {'post': post,
+                       'similar_posts': similar_posts})
