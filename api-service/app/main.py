1512877590eb2379eeca818405bfa421e208dec3
SergeyGalanin <galaninss.del@gmail.com>
2024-05-08 12:21:33 +0300

    feat: "created and connected database"

--- api-service/app/main.py
+++ api-service/app/main.py
@@ -1,13 +1,8 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
-def read_root():
-    return {"message": "Hello World!"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+async def root():
+    return {"message": "Hello World"}
