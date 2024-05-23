3890dd804248e01d93a59b7dbef44a42e9355e84
Ilya Rutski <teror714uns@gmail.com>
2024-05-23 11:25:13 +0300

    feature: clients detail graph - data diapason
    сделал диапазон для выбора дат для составления графика

--- mrs_proj/clients/templates/clients/graph_scripts.js
+++ mrs_proj/clients/templates/clients/graph_scripts.js
@@ -2,11 +2,15 @@ const showChartBtn = document.getElementById('show-chart-btn');
 const hideChartBtn = document.getElementById('hide-chart-btn');
 const chartContainer = document.getElementById('parameter-chart-container');
 const parameterSelect = document.getElementById('parameter-select');
+const startDatePicker = document.getElementById('start-date');
+const endDatePicker = document.getElementById('end-date');
 
 
 showChartBtn.addEventListener('click', function () {
     const currentParameter = parameterSelect.value;
-    const graph_url_param = `${graph_url}?parameter=${currentParameter}`;
+    const startDate = startDatePicker.value;
+    const endDate = endDatePicker.value;
+    const graph_url_param = `${graph_url}?parameter=${currentParameter}&start_date=${startDate}&end_date=${endDate}`;
 
     fetch(graph_url_param)
         .then(response => response.json())
