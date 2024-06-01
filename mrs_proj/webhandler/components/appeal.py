80002d23664b448b318d38f72ba3f4e4108c4a34
Ilya Rutski <teror714uns@gmail.com>
2024-06-01 21:07:06 +0300

    update: web-handler help(styles)
    обновил стили для этого отображения

--- mrs_proj/webhandler/components/appeal.py
+++ mrs_proj/webhandler/components/appeal.py
@@ -1,17 +1,18 @@
 from django_unicorn.components import UnicornView
 from webhandler.models import SupportTicket
 
-
 class AppealView(UnicornView):
     email = ""
     message = ""
     open_tickets = []
+    closed_tickets = []
 
     def mount(self):
-        self.load_open_tickets()
+        self.load_tickets()
 
-    def load_open_tickets(self):
+    def load_tickets(self):
         self.open_tickets = SupportTicket.objects.filter(user=self.request.user, status="open")
+        self.closed_tickets = SupportTicket.objects.filter(user=self.request.user, status="closed")
 
     def add_comment(self):
         if self.email and self.message:
@@ -23,7 +24,7 @@ class AppealView(UnicornView):
             )
             self.email = ""
             self.message = ""
-            self.load_open_tickets()  # Reload open tickets after adding a comment
+            self.load_tickets()  # Reload tickets after adding a comment
             self.call("alert", "Комментарий добавлен успешно!")
         else:
             self.call("alert", "Заполните все поля!")
